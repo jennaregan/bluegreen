2ec699fbfbb8407716a1d7b1ec3a170e97c49ace
jgammon612 <jgammon@redhat.com>
2019-05-03 16:52:16 -0500

    added red

--- image.php
+++ image.php
@@ -8,6 +8,7 @@ $green = imagecolorallocate($im, 0, 255, 0);
 // Draw a filled rectangle
 //imagefilledrectangle($im, 0, 0, 199, 199, $blue);
 imagefilledrectangle($im, 0, 0, 199, 199, $green);
+imagefilledrectangle($im, 0, 0, 255 ,255, $red);
 
 // Save the image
 header('Content-Type: image/png');
